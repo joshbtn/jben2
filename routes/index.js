714073061f8998cd8252e42e765eac906fc0afae
josh <josh@jextoo.westell.com>
2012-12-31 16:39:09 -0500

    got twitter feed pulling in

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,43 @@
-
+var http = require('http'),
+    async = require('async');
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', 
-  {
-	  title: "Josh Bennett | Philadelphia web and interactive developer"
-	  , description: "I'm a Philadelphia interactive solutions developer specializing in web, mobile and interactive applications."
-	  , share_url: "http://j-ben.com"
-	  , image_url: "http://www.gravatar.com/avatar/dccdd2e02b7979f8b48f96a91376e7eb.png"
-	  , og_type: "website"
-  });
+exports.index = function (req, res) {
+    async.series([
+    getTweets],
+    // optional callback
+    function (err, results) {
+        res.render('index', {
+            title: "Josh Bennett | Philadelphia web and interactive developer",
+            description: "I'm a Philadelphia interactive solutions developer specializing in web, mobile and interactive applications.",
+            share_url: "http://j-ben.com",
+            image_url: "http://www.gravatar.com/avatar/dccdd2e02b7979f8b48f96a91376e7eb.png",
+            og_type: "website",
+            tweets: results[0]
+        });
+    });
 };
 
 
-function getTweets(){
-	//This is the url for user info
-	//https://api.twitter.com/1/statuses/user_timeline.json?include_entities=false&include_rts=true&screen_name=joshatjben&count=5
+function getTweets(callback) {
+    //http://api.twitter.com/1/statuses/user_timeline.json?screen_name=joshatjben
+    http.get(
+    	"http://api.twitter.com/1/statuses/user_timeline.json?screen_name=joshatjben",
+    	function(res){
+			var data = '';
+		
+		    res.on('data', function (chunk) {
+		        data += chunk;
+		    });
+		
+		    res.on('end', function () {
+		        var obj = JSON.parse(data);
+		        console.log(obj);
+		        callback(null, obj);
+		    });
+		    
+		    
+	    });
 }
